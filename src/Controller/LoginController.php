653d9285328fbae92dc2d2af9ec6816377243301
WhidanB <danyphone95@gmail.com>
2023-08-10 13:00:33 +0200

    route index

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-            return $this->redirectToRoute('/task');
+            return $this->redirectToRoute('app_task_index');
         }
 
         // get the login error if there is one
